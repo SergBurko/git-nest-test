07ccf1d2d8c7ca832b4a7b526a9e2719e3d5623d
SergBurko <opponib2@gmail.com>
2021-02-07 18:12:52 +0300

    Main. one more time

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '!!Hello Mel!!xbgjstsgsnfddfhdshdhnhfgjfxddklgndklfmh';
-  }
+    return '!!Hello Mel!! CLEAR SOME DATA';
+}
+
+
 }
