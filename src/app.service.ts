bcc678e9252b6330d880ea3b776233d6cb0691a1
Melonorm <shvagerv@gmail.com>
2021-02-07 17:22:06 +0200

    555

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,9 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return '!!Hello Mel!! CLEAR SOME DATA';
-}
+  a: number = 6
+  b: number = 10
 
+  getHello(): string {
+
+    return '!!Hello Mel!!return !!Hello Mel!! CLEAR SOME DATA' + (this.a + this.b);
+  }
 
 }
