a401a852e16a55d6c2af8a89a4fa85f5b74af9d7
Melonorm <shvagerv@gmail.com>
2021-02-07 17:01:45 +0200

    324144

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '!!Hello Mel!!xbgjstsgsnfdnhfgjfxddklgndklfmh';
+    return '!!Hello Mel!!xbgjstsgsnfddfhdshdhnhfgjfxddklgndklfmh';
   }
 }
